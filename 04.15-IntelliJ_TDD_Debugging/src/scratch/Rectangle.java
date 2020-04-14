b7ebcd9a3c69d35647bc4372dbb66b763c929619
Brian Cui <brian.cui@live.com>
2020-04-13 19:40:04 -0700

    Add initial 04.15 notes

---
+++ 04.15-IntelliJ_TDD_Debugging/src/scratch/Rectangle.java
@@ -0,0 +1,39 @@
+package scratch;
+
+public class Rectangle implements Shape2D {
+    int width;
+    int height;
+
+    // Constructor
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    // Methods
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public void doubleDimensions() {
+        this.width = this.width * 2;
+        this.height = this.height * 2;
+    }
+
+    @Override
+    public double area() {
+        return (double) this.width * (double) this.height;
+    }
+}
