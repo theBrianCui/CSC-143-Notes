f4a187e550f2f6ea130725bdad11f23bad90a2ef
Brian Cui <brian.cui@live.com>
2020-09-24 19:31:19 -0700

    Replace recursion with iteration

--- 04.13-Introductions_Cmd_Git/Fibonacci.java
+++ 04.13-Introductions_Cmd_Git/Fibonacci.java
@@ -9,14 +9,14 @@ public class Fibonacci {
     public static int[] computeRange(int upToIndex) {
         int[] fib = new int[upToIndex];
         for (int i = 0; i < upToIndex; ++i) {
-            fib[i] = computeRecursive(i);
+            fib[i] = fibonacci(i);
         }
 
         return fib;
     }
 
     // O(2^n)
-    public static int computeRecursive(int n) {
+    public static int fibonacci(int n) {
         if (n == 0) {
             return 0;
         }
@@ -25,6 +25,15 @@ public class Fibonacci {
             return 1;
         }
 
-        return computeRecursive(n - 1) + computeRecursive(n - 2);
+        int first = 0;
+        int second = 1;
+        int next = first + second;
+        for (int i = 2; i <= n; ++i) {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+
+        return next;
     }
 }
