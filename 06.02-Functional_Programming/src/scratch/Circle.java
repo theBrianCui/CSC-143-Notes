0236c737f6bc1e33fc81e63b6ee7d3c08f473bf6
Brian Cui <brian.cui@live.com>
2021-06-01 21:59:12 -0700

    Add 06.02 notes

---
+++ 06.02-Functional_Programming/src/scratch/Circle.java
@@ -0,0 +1,51 @@
+package scratch;
+
+public class Circle implements Comparable<Circle> {
+    public int radius;
+    public String name;
+
+    public Circle(int radius) {
+        this.radius = radius;
+        this.name = "";
+    }
+
+    public Circle(int radius, String name) {
+        this.radius = radius;
+        this.name = name;
+    }
+
+    // Specifies the natural order of Circle as by radius
+    // Ascending based on radius
+    @Override
+    public int compareTo(Circle o) {
+        if (radius == o.radius) {
+            return 0;
+        }
+
+        if (radius < o.radius) {
+            return -1;
+        }
+
+        return 1;
+
+        // return Integer.compare(radius, o.radius);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+
+        if (this == o) {
+            return true;
+        }
+
+        if (this.getClass() != o.getClass()) {
+            return false;
+        }
+
+        Circle other = (Circle) o;
+        return this.radius == other.radius && this.name.equals(other.name);
+    }
+}
